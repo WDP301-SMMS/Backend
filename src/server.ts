703ee024a82196492706dc9141a5056a3cf800ca
Khai_Tran <tranmaiquangkhai@gmail.com>
2025-06-03 22:20:54 +0700

    init

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import 'module-alias/register';
+import app from './app';
+import config from '@config/config';
+
+app.listen(config.port, () => {
+  console.log('Server is running on port', config.port);
+  console.log(`Environment: ${config.nodeEnv}`);
+});
