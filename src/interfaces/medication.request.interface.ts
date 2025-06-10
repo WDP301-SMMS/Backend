0bff8a832b4b4939df041e62b7a3cf26197b3fb0
realjeremy04 <tuankietvt04@gmail.com>
2025-06-10 12:52:18 +0700

    feat: medication request's model

---
+++ src/interfaces/medication.request.interface.ts
@@ -0,0 +1,16 @@
+import { Types } from "mongoose";
+
+export interface IMedicationRequest {
+  parentId: Types.ObjectId;
+  startDate: Date;
+  endDate: Date;
+  prescriptionFile: string;
+  status: string;
+}
+
+export interface IRequestItem {
+  requestId: Types.ObjectId;
+  medicationName: string;
+  dosage: string;
+  instruction: string;
+}
---
+++ src/models/medication.request.model.ts
@@ -0,0 +1,50 @@
+import { IMedicationRequest, IRequestItem } from "@/interfaces/medication.request.interface";
+import mongoose, { Schema } from "mongoose";
+
+
+const MedicationRequestSchema = new Schema<IMedicationRequest>({
+    parentId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    prescriptionFile: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+});
+
+const RequestItemSchema = new Schema<IRequestItem>({
+    requestId: {
+        type: Schema.Types.ObjectId,
+        ref: "MedicationRequest",
+        required: true
+    },
+    medicationName: {
+        type: String,
+        required: true
+    },
+    dosage: {
+        type: String,
+        required: true
+    },
+    instruction: {
+        type: String,
+        required: true
+    },
+});
+
+export const MedicationRequestModel = mongoose.model<IMedicationRequest>("MedicationRequest", MedicationRequestSchema);
+export const RequestItemModel = mongoose.model<IRequestItem>("RequestItem", RequestItemSchema);
