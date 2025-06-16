6f228d2ef3041c86358f172177eeefab309f3bc7
Khai_Tran <tranmaiquangkhai@gmail.com>
2025-06-16 14:25:04 +0700

    feat: init swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-import express from "express";
-import { errorHandler } from "./middlewares/globalErrorHandler";
-import connectDB from "./config/database";
-import "dotenv/config";
+import express from 'express';
+import { errorHandler } from './middlewares/globalErrorHandler';
+import connectDB from './config/database';
+import 'dotenv/config';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 connectDB();
+
+const swaggerDocument = YAML.load('swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
