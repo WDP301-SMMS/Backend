3342c6a519602ca8e3b7c766417f1c18813446e0
Kevin Truong <kevintruong6587@gmail.com>
2025-07-27 01:19:17 +0700

    fix status

--- src/controllers/health-check/health-check-record.controller.ts
+++ src/controllers/health-check/health-check-record.controller.ts
@@ -145,7 +145,7 @@ const getStudentHealthCheckRecord = async (req: Request, res: Response) => {
     })
       .populate('resultId')
       .populate('studentId');
-    if (!records || records.length === 0) {
+    if (!records) {
       res.status(404).json({ message: 'Record not found' });
       return;
     }
